37139ff3f1138b7704b9d4fd44cf5d590fa25bd2
Dustin Evans <dustin@omnibond.com>
2016-05-02 12:22:24 -0400

    Finished first version of pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -9,28 +9,69 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from datetime import datetime, timedelta
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import BaseDocTemplate, Frame, PageTemplate, Image
+from reportlab.lib.units import mm
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from spreadsheettable import SpreadsheetTable
+
+styleSheet = getSampleStyleSheet()
+MARGIN_SIZE = 5 * mm
+PAGE_SIZE = A4
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 def write_data(data_dict, dest_filename):
-    canvas = canvas.Canvas(dest_filename, pagesize=letter)
-    width, height = letter
+    table_style = [
+        ('GRID', (0,0), (-1,-1), 1, colors.black),
+        ('ALIGN', (0,0), (-1,-1), 'CENTER'),
+        ('VALIGN', (0,0), (-1,-1),'MIDDLE'),
+        ('LEFTPADDING', (0,0), (-1,-1), 3),
+        ('RIGHTPADDING', (0,0), (-1,-1), 3),
+        ('FONTSIZE', (0,0), (-1,-1), 10),
+        ('FONTNAME', (0,0), (-1,0), 'Times-Bold'),
+    ]
+
+    data = [
+        ['Bay', 'Start Time', 'Service Time', 'Wait Time', 'Total Customer Time', 'Snapshot']
+    ]
+
+    vehicles = data_dict['vehicles']
+    for obj in vehicles:
+        bay = obj['bay']
+        start_time = datetime.fromtimestamp(obj['t_enter']).strftime('%I:%M %p')
+        service_time = abs(obj['t_leave'] - obj['t_enter'])
+        wait_time = abs(obj['t_enter'] - obj['t_queue_enter'])
+        total_customer_time = service_time + wait_time
+        snapshot = Image(obj['snapshot'], 40*mm, 30*mm)
+
+        col_entries = [bay, start_time, str(timedelta(seconds=service_time)), str(timedelta(seconds=wait_time)), str(timedelta(seconds=total_customer_time)), snapshot]
+        data.append(col_entries)
+
+    story = []
+    spreadsheet_table = SpreadsheetTable(data, repeatRows = 1)
+    spreadsheet_table.setStyle(table_style)
+    story.append(spreadsheet_table)
+
+    create_pdfdoc(dest_filename, story)
 
 
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-def draw_header(source_canvas, letter_width, letter_height):
-    headers = ['Bay', 'Start Time', 'Service Time', 'Wait Time', 'Total Customer Time', 'Snapshot']
-    for i in xrange(0, len(headers)):
-        x = i * 100
-        if i != len(headers)-1:
-            # Text header
-            pass
-        else:
-            # Snapshot header
-            pass
+def create_pdfdoc(dest_filename, story):
+    pdf_doc = BaseDocTemplate(dest_filename, pagesize=PAGE_SIZE,
+        leftMargin=MARGIN_SIZE, rightMargin=MARGIN_SIZE,
+        topMargin=MARGIN_SIZE, bottomMargin=MARGIN_SIZE)
+    main_frame = Frame(MARGIN_SIZE, MARGIN_SIZE,
+        PAGE_SIZE[0] - 2 * MARGIN_SIZE, PAGE_SIZE[1] - 2 * MARGIN_SIZE,
+        leftPadding = 0, rightPadding = 0, bottomPadding = 0,
+        topPadding = 0, id = 'main_frame')
+    main_template = PageTemplate(id = 'main_template', frames = [main_frame])
+    pdf_doc.addPageTemplates([main_template])
+
+    pdf_doc.build(story)
