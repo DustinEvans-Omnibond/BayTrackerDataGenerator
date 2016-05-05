386e8d984cb64c44e2b7580d6f2523c7186f6c2b
Dustin Evans <dustin@omnibond.com>
2016-05-05 10:30:54 -0400

    Updated pdf_generator for headers/footers meta data

--- pdf_generator.py
+++ pdf_generator.py
@@ -12,20 +12,31 @@
 from pytz import timezone
 from datetime import datetime, timedelta
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import BaseDocTemplate, Frame, PageTemplate, Image
+from reportlab.platypus import BaseDocTemplate, Frame, PageTemplate, Image, Paragraph, Spacer
 from reportlab.lib.units import mm
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib import colors
 from spreadsheettable import SpreadsheetTable
 
 styleSheet = getSampleStyleSheet()
-MARGIN_SIZE = 5 * mm
+SIDE_MARGIN_SIZE = 23 * mm
+VERT_MARGIN_SIZE = 5 * mm
 PAGE_SIZE = A4
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 def write_data(data_dict, dest_filename, default_timezone):
+    story = []
+
+    # Create meta info headers if available
+    if 'headers' in data_dict:
+        meta_headers = data_dict['headers']
+        for header in meta_headers:
+            story.append(Paragraph('<font size=12>' + header + '</font>', styleSheet['Normal']))
+            story.append(Spacer(1, 12))
+
+    # Create data table
     table_style = [
         ('GRID', (0,0), (-1,-1), 1, colors.black),
         ('ALIGN', (0,0), (-1,-1), 'CENTER'),
@@ -35,10 +46,6 @@ def write_data(data_dict, dest_filename, default_timezone):
         ('FONTSIZE', (0,0), (-1,-1), 10),
         ('FONTNAME', (0,0), (-1,0), 'Times-Bold'),
     ]
-
-    # Get meta info headers
-    if 'headers' in data_dict:
-        meta_headers = data_dict['headers']
 
     data = [
         ['Bay', 'Start Time', 'Service Time', 'Wait Time', 'Total Customer Time', 'Snapshot']
@@ -59,11 +66,20 @@ def write_data(data_dict, dest_filename, default_timezone):
         col_entries = [bay, start_time, str(timedelta(seconds=service_time)), str(timedelta(seconds=wait_time)), str(timedelta(seconds=total_customer_time)), snapshot]
         data.append(col_entries)
 
-    story = []
     spreadsheet_table = SpreadsheetTable(data, repeatRows = 1)
     spreadsheet_table.setStyle(table_style)
     story.append(spreadsheet_table)
 
+
+    # Create footers info if available
+    if 'footers' in data_dict:
+        footers = data_dict['footers']
+        for footer in footers:
+            story.append(Paragraph('<font size=12>' + footer + '</font>', styleSheet['Normal']))
+            story.append(Spacer(1, 12))
+
+
+    # Save everything to PDF Doc
     create_pdfdoc(dest_filename, story)
 
 
@@ -73,10 +89,10 @@ def write_data(data_dict, dest_filename, default_timezone):
 #-------------------------------------------------------------------------------
 def create_pdfdoc(dest_filename, story):
     pdf_doc = BaseDocTemplate(dest_filename, pagesize=PAGE_SIZE,
-        leftMargin=MARGIN_SIZE, rightMargin=MARGIN_SIZE,
-        topMargin=MARGIN_SIZE, bottomMargin=MARGIN_SIZE)
-    main_frame = Frame(MARGIN_SIZE, MARGIN_SIZE,
-        PAGE_SIZE[0] - 2 * MARGIN_SIZE, PAGE_SIZE[1] - 2 * MARGIN_SIZE,
+        leftMargin=SIDE_MARGIN_SIZE, rightMargin=SIDE_MARGIN_SIZE,
+        topMargin=VERT_MARGIN_SIZE, bottomMargin=VERT_MARGIN_SIZE)
+    main_frame = Frame(SIDE_MARGIN_SIZE, VERT_MARGIN_SIZE,
+        PAGE_SIZE[0] - 2 * SIDE_MARGIN_SIZE, PAGE_SIZE[1] - 2 * VERT_MARGIN_SIZE,
         leftPadding = 0, rightPadding = 0, bottomPadding = 0,
         topPadding = 0, id = 'main_frame')
     main_template = PageTemplate(id = 'main_template', frames = [main_frame])
